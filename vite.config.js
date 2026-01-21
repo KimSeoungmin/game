121bf51717cf708b609bb47f9ce2c03f9a348ef8
SeungMinKim94 <ksm2003k2@quve.kr>
2026-01-21 09:21:00 +0900

    modify: vite.config.js
    add: koybe allow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    host: '0.0.0.0',
+    port: 8000,
+    allowedHosts: [
+      'new-marleah-kimgame-2b677a93.koyeb.app',
+      '.koyeb.app', // 모든 koyeb.app 서브도메인 허용
+    ],
+  },
   plugins: [react()],
 })
